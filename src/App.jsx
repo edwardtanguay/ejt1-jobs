f50f73cbe4604eb28f4c013122ffe9382443c53d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-23 11:21:49 +0200

    .env file in

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss'
 
-const backend_url = 'http://localhost:3033/jobs';
+const backend_url = import.meta.env.VITE_BACKEND_URL; 
 
 function App() {
   const [jobs, setJobs] = useState([]);
@@ -12,7 +12,7 @@ function App() {
       setJobs((await axios.get(backend_url)).data);
     })();
   }, []);
-  
+
   return (
     <div className="App">
       <h1>Jobs</h1>
